a599d0abcdbf56086fbd12cf74af93d8e1f423df
Evgen-ios <jack38irk@icloud.com>
2021-02-08 22:43:43 +0800

    Start

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var colorLigh = 0
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        switchlight(number: colorLigh)
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        switchlight(number: colorLigh)
+        
+    }
+    
+//    MARK: Switch Light
+    func switchlight(number: Int) {
+        switch number {
+        case 1:
+            view.backgroundColor = .green
+        case 2:
+            view.backgroundColor = .red
+        case 3:
+            view.backgroundColor = .blue
+        default:
+            view.backgroundColor = .white
+            colorLigh = 0
+        }
+        
+        colorLigh += 1
+    }
 }
-
